1b09fffe46d209635080d6baf24d177a79e5e87d
ibrahim <your-email@example.com>
2025-05-13 10:02:43 +0300

    Fix plus_two function and ensure tests pass

---
+++ testing/ipdb_debugging_test.py
@@ -0,0 +1,8 @@
+import ipdb
+import pytest
+from lib.ipdb_debugging import plus_two
+
+class TestIpdbDebugging:
+    def test_adds_two(self):
+        '''adds_two() adds 2 to input arg and returns sum'''
+        assert plus_two(3) == 5
